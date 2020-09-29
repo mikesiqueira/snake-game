5d2b0871b68f31aac25e678528a102fe8f858a59
Mike Siqueira <mikesiqueira.ti@gmail.com>
2020-09-29 12:11:31 -0300

    Initial Commit

---
+++ src/snake/Application.java
@@ -0,0 +1,10 @@
+package snake;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		 Game g = new Game();
+		 g.start();
+	}
+
+}
---
+++ src/snake/GameUtils.java
@@ -0,0 +1,10 @@
+package snake;
+
+public enum GameUtils {
+	
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+
+}
